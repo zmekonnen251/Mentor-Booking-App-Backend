110e6f360b0663ca6f6183c9ad18bc7b8d7d6ee3
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-21 22:15:13 +0300

    Created devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  include Users::Allowlist
+
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: self
+
+  has_many :allowlisted_jwts, dependent: :destroy
+
+  def for_display
+    {
+      id:,
+      email:
+    }
+  end
+end
