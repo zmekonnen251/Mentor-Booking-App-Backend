eba277eb6a2e6b2791982be5793599b932979546
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-26 00:40:27 +0300

    Add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,15 @@
 class User < ApplicationRecord
   include Users::Allowlist
+  ActiveStorage::Current.url_options = -> { { host: 'localhost', port: 3000 } }
 
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :jwt_authenticatable,
          jwt_revocation_strategy: self
 
   has_many :allowlisted_jwts, dependent: :destroy
-  
+
   has_many :bookings, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  # def for_display
-  #   {
-  #     id:,
-  #     email:
-  #   }
-  # end
+  has_one_attached :avatar
 end
