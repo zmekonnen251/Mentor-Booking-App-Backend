a8d86874eacf56d23721977c6f8c2ba6b3809144
zieeco <zieecosam@gmail.com>
2022-08-31 14:42:26 +0100

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_171153) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_213124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_171153) do
     t.bigint "mentor_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country", null: false
+    t.string "city", null: false
     t.index ["mentor_id"], name: "index_bookings_on_mentor_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
