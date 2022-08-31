3d9734d8ea952cd25bb7638a9c269c9d07267a47
zieeco <zieecosam@gmail.com>
2022-08-31 14:43:03 +0100

    Update the reserve controller

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -7,16 +7,28 @@ class Api::V1::BookingsController < ApplicationController
   end
 
   def cancel_reservation
-    
+
   end
 
   def reserve
-    puts 'you are in the reservation'
+    @mentor = Mentor.find(booking_params[:mentor_id].to_i)
+    @user= User.find(booking_params[:user_id].to_i)
+    @city = booking_params[:city]
+    @country = booking_params[:country]
+    @date = booking_params[:date]
+
+    @booking = Booking.create(mentor: @mentor, user: @user, city: @city, date: @date, country: @country)
+
+    if @booking.save
+      render json: { message: 'Booking created successfully.' }, status: :ok
+    else
+      render json: { message: 'Booking not created.' }, status: :unprocessable_entity
+    end
   end
 
   private
-  
+
   def booking_params
-    params.permit(:mentor_id, :user_id)
+    params.permit(:mentor_id, :user_id, :city, :country, :date)
   end
 end
