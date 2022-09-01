9647b40bb9c35ada4119f901cca7390502451828
Adekunle Ismail <adismail4@gmail.com>
2022-09-01 22:31:04 +0100

    Add like test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
+
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+ let(:mentor) {Mentor.create(email: 'user@yahoo.com', password: 'password', name: 'mentor')}
+ let(:user) {User.create(email: 'user@yahoo.com', password: 'password', name: 'user')}
+  subject{Like.create(id: 1, mentor_id: mentor.id, user_id: user.id)}
+
+  
+  it "should correctly identify the user relationship" do
+    subject.user_id = user.id
+    expect(subject).to be_valid    
+  end
+
+  it "should correctly identify the mentor relationship" do
+    subject.mentor_id = mentor.id
+    expect(subject).to be_valid    
+  end
 end
