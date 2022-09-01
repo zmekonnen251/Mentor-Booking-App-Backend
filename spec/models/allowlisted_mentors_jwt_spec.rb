54ac51dc3e494022832700862852288f5f4d1bf5
Adekunle Ismail <adismail4@gmail.com>
2022-09-01 13:08:24 +0100

    Use rspec to test AllowlistedMentorsJwt model

--- spec/models/allowlisted_mentors_jwt_spec.rb
+++ spec/models/allowlisted_mentors_jwt_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe AllowlistedMentorsJwt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:allowlisted_mentors_jwts) {AllowlistedMentorsJwt.create(id: 1, jti: 'jti', aud: 'aud', exp: '1 hour',remote_ip: 'ip', browser_data: 'data', os_data: 'os', device_data: 'devise')}
+ it 'is valid with jti' do
+    subject.jti = 'jti'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with devise data' do
+    subject.device_data = 'devise'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with os data' do
+    subject.os_data = 'os'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with browser data' do
+    subject.browser_data = 'data'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with remote ip' do
+    subject.remote_ip = 'ip'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with exp' do
+    subject.exp = '1 hour'
+    expect(subject).to be_valid
+  end
+
+  it 'should have 1 as valid id' do
+    subject.id = 1
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with aud' do
+    subject.aud = 'aud'
+    expect(subject).to be_valid
+  end
+
+
+
+
 end
+
+
