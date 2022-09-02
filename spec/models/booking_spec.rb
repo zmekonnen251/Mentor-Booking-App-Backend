bd0ae904975e4afd401d7cdc2650be6de9d9a682
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-09-02 11:03:31 +0300

    Revert "Rspec test"

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,36 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  let(:mentor) {Mentor.create(email: 'mentor@yahoo.com', password: 'password', name: 'mentor')}
-  let(:user) {User.create(email: 'user@yahoo.com', password: 'password', name: 'user')}
-
-  subject do
-    described_class.new(date: '2022-09-01', country: 'nigeria', city: 'lagos', user_id: user.id, mentor_id: mentor.id)
-  end
-
-  it "should correctly identify the mentor relationship" do
-    subject.mentor_id = mentor.id
-    expect(subject).to be_valid    
-  end
-
-  it "should correctly identify the user relationship" do
-    subject.user_id = user.id
-    expect(subject).to be_valid    
-  end
-
-  it "should not be valid without date " do
-    subject.date = ''
-    expect(subject).to_not be_valid    
-  end
-
-  it "should be valid with country " do
-    subject.country = 'nigeria'
-    expect(subject).to be_valid    
-  end
-
-  it "should be valid with city " do
-    subject.city = 'lagos'
-    expect(subject).to be_valid    
-  end
- 
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/mentor_technology_spec.rb
+++ spec/models/mentor_technology_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe MentorTechnology, type: :model do
-  let(:mentor) {Mentor.create(email: 'user@yahoo.com', password: 'password', name: 'mentor')}
-  let(:technology) {Technology.create(name: 'technology')}
-  subject{MentorTechnology.create(mentor_id: mentor.id, technology_id: technology.id)}
-
-   it "should correctly identify the technology relationship" do
-    subject.technology_id = technology.id
-    expect(subject).to be_valid    
-  end
-
-  it "should correctly identify the mentor relationship" do
-    subject.mentor_id = mentor.id
-    expect(subject).to be_valid    
-  end
-
-  
+  pending "add some examples to (or delete) #{__FILE__}"
 end
