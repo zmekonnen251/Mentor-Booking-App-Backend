af82bb026b5e1d4d17a14e261419160a73e0ec28
Zelalem <mekonnen.zelalem251@gmail.com>
2023-01-11 18:17:05 +0300

    Deployed to fly.io

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# user = User.create(name: 'admin',email: 'zolget86@gmail.com',password:'Z0|a@3783',role:'superadmin')
+
+Mentor.destroy_all
+
+Mentor.create([{                                                 
+  email: "kalumunabisimwa5@gmail.com",                        
+  bio: "Full-Stack Web Developer",                            
+  name: "Alfred Bisimwa",                                     
+  phone: "+251924561527",                                     
+  approved: true,                                             
+  img_url: "https://avatars.githubusercontent.com/u/88894525?v=4"
+},
+{
+  email: "zieecosam@gmail.com",
+  bio: "Full-Stack Developer",
+  name: "Isaac Imaobong Samuel",
+  phone: "+251924561527",
+  approved: true,
+  img_url: "https://avatars.githubusercontent.com/u/59397159?v=4"
+},
+ { 
+  email: "wuletawwonte@gmail.com",
+  bio: "Full-stack developer",
+  name: "Wuletaw Wonte",
+  phone: "+251924561527",
+  approved: true,
+  img_url: "https://avatars.githubusercontent.com/u/12524453?v=4"
+},
+{
+  email: "adismail4@gmail.com",
+  bio: "Full-Stack web Developer",
+  name: "Ismail Akinkunmi",
+  phone: "+251924561527",
+  approved: true,
+  img_url: "https://avatars.githubusercontent.com/u/37457094?v=4"
+},
+{ 
+  email: "me1@gmail.com",
+  bio: "Full stack developer",
+  name: "Zelalem Mekonnen",
+  phone: "+251924561527",
+  approved: true,
+  img_url: "https://avatars.githubusercontent.com/u/37457094?v=4"
+}
+])
+
+p "Created #{Mentor.count} Mentors"
